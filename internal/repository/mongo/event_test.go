3e99f32c6143333be6095655323ef22e9d257b85
Mathias Roberto Back <mathiasrback@gmail.com>
2025-10-21 23:33:00 -0300

    test: improve event repository

--- internal/repository/mongo/event_test.go
+++ internal/repository/mongo/event_test.go
@@ -57,7 +57,7 @@ func TestEventRepository_Insert(t *testing.T) {
 
 	defer client.Database().Collection("events").Drop(ctx)
 
-	t.Run("should upsert event successfully", func(t *testing.T) {
+	t.Run("should insert event successfully", func(t *testing.T) {
 		event := domain.Event{
 			Name: "Test Event",
 			Date: &time.Time{},
@@ -66,5 +66,64 @@ func TestEventRepository_Insert(t *testing.T) {
 		id, err := rep.Insert(ctx, event)
 		assert.NoError(t, err)
 		assert.NotEmpty(t, id)
+	})
+}
+
+func TestEventRepository_Delete(t *testing.T) {
+	rep := mongo.NewEventRepository(client, zap.NewNop())
+
+	defer client.Database().Collection("events").Drop(ctx)
+
+	t.Run("should delete event successfully", func(t *testing.T) {
+		event := domain.Event{
+			Name: "Test Event",
+			Date: &time.Time{},
+		}
+
+		id, err := rep.Insert(ctx, event)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, id)
+
+		err = rep.Delete(ctx, id)
+		assert.NoError(t, err)
+
+		res, err := rep.FindByID(ctx, id)
+		assert.Error(t, err)
+		assert.Nil(t, res)
+	})
+}
+
+func TestEventRepository_Update(t *testing.T) {
+	rep := mongo.NewEventRepository(client, zap.NewNop())
+
+	defer client.Database().Collection("events").Drop(ctx)
+
+	t.Run("should delete event successfully", func(t *testing.T) {
+		event := domain.Event{
+			Name: "Test Event",
+			Date: &time.Time{},
+		}
+
+		id, err := rep.Insert(ctx, event)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, id)
+
+		date := time.Now().UTC().Add(100 * time.Hour)
+		event.ID = id
+		event.Name = "Updated Event"
+		event.Date = &date
+		err = rep.Update(ctx, event)
+		assert.NoError(t, err)
+
+		res, err := rep.FindByID(ctx, id)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, res)
+		assert.Equal(t, "Updated Event", res.Name)
+		assert.Equal(t, date.Day(), res.Date.Day())
+		assert.Equal(t, date.Month(), res.Date.Month())
+		assert.Equal(t, date.Year(), res.Date.Year())
+		assert.Equal(t, date.Hour(), res.Date.Hour())
+		assert.Equal(t, date.Minute(), res.Date.Minute())
+		assert.Equal(t, date.Second(), res.Date.Second())
 	})
 }
